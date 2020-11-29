25bdff3e1c5f5fd69c4f88e4e469ef734457d131
beckyphan <beckyphan@live.com>
2020-11-29 09:27:36 -0800

    changed App.js to class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      App.js
-    </div>
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        App.js
+      </div>
   );
 }
 
