a601e0f1c1e546a84befde164c396393e9655f18
beckyphan <beckyphan@live.com>
2020-11-29 09:26:17 -0800

    created placeholder navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = (props) => {
+
+  return (
+    <div class="nav">
+      Navigation Bar Links Here
+    </div>
+  )
+}
