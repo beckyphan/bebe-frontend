263c53287772d1ee4d6a0a59253f6591c808d1cf
beckyphan <beckyphan@live.com>
2020-12-06 20:59:40 -0800

    added conditional display to NavBar depending on if user is logged in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,22 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = (props) => {
 
-  return (
-    <div className="nav">
-       <NavLink exact activeClassName="active" to="/login">Login</NavLink>
-       <NavLink exact activeClassName="active" to="/register">Register</NavLink>
-  </div>
-  )
+  const isLoggedIn = !!props.user.id
+
+  if (isLoggedIn) {
+    return(
+      <div className="nav">
+        Welcome, {props.user.attributes.name}!
+      </div>
+    )
+  } else {
+    return (
+      <div className="nav">
+           <NavLink exact activeClassName="active" to="/login">Login</NavLink>
+           <NavLink exact activeClassName="active" to="/register">Register</NavLink>
+      </div>
+    )
+  }
 }
 
 export default NavBar
