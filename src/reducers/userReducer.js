1c268b7a3c92fe20e3c7b726c7fb3a7dc7cab0e4
beckyphan <beckyphan@live.com>
2020-12-12 21:46:59 -0800

    created FETCH_BEBES to return array of bebes fr dispatch action payload

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,13 +1,20 @@
-export default function userReducer(state = {user: []}, action) {
+export default function userReducer(state = {user: [], bebes: []}, action) {
 
   switch(action.type) {
     case 'LOGIN_USER':
-      console.log(action.payload.data)
+      // console.log(action.payload.data)
       return { user: action.payload.data }
 
     case 'REGISTER_USER':
-      console.log(action.payload.data)
+      // console.log(action.payload.data)
       return { user: action.payload.data }
+
+    case 'FETCH_BEBES':
+      console.log("C")
+      return {
+        ...state,
+        bebes: action.payload.data
+      }
 
     default:
       return state
