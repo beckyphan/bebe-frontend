b362d7fd8d798b0d50a4b28575949d9fbbd4ad86
beckyphan <beckyphan@live.com>
2020-12-12 22:13:20 -0800

    map through fetched bebes passed as props to Bebes to display each

--- src/components/Bebes.js
+++ src/components/Bebes.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Bebes = (props) => {
 
+  function bebeCard(bebe) {
+    return (
+      <div key={bebe.id} className="card">
+        <h3>{bebe.attributes.name}</h3>
+        <p className="align-left">
+          Kind: {bebe.attributes.kind} <br/>
+          Birthdate: {bebe.attributes.birthdate} <br/>
+          Bio: {bebe.attributes.bio}</p>
+      </div>
+    )
+  }
+
   return (
     <div className="dashboard">
-      SHOW BEBES HERE
+      {props.bebes.map(bebe => bebeCard(bebe))}
     </div>
   )
 
