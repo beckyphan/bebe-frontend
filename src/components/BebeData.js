b1614bc3ac858b1448684160adbdcfbe97e5609f
beckyphan <beckyphan@live.com>
2020-12-20 18:24:58 -0800

    BebeData displays bebe info, will also show associated data here

---
+++ src/components/BebeData.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BebeData = (props) => {
+  console.log("in BebeData")
+  console.log(props)
+
+  return (
+    <div className="show-bebe">
+      <h1>{props.bebe.attributes.name}</h1>
+      <img src={props.bebe.attributes.img} alt="baby pic" className="headshot"/>
+      <p className="align-left">
+        Kind: {props.bebe.attributes.kind} <br/>
+        Birthdate: {props.bebe.attributes.birthdate} <br/>
+        Bio: {props.bebe.attributes.bio} </p>
+    </div>
+  )
+}
+
+export default BebeData
