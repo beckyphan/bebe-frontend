65e2a686331cad01ea731bdfad3a80a2bd278d9b
beckyphan <beckyphan@live.com>
2020-12-29 11:57:58 -0800

    created deleteBebe action

---
+++ src/actions/deleteBebe.js
@@ -0,0 +1,15 @@
+export const deleteBebe = (bebeId, userId) => {
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/users/' + userId + '/bebes/' + bebeId, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => response.json())
+    .then(bebe => {
+        console.log(bebe)
+        dispatch({type: 'DELETE_BEBE', payload: bebe})
+    })
+  }
+}
