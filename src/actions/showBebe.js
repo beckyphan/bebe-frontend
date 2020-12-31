2d7f7d5ffd2b8ce527676a904e608513ce3ec28c
beckyphan <beckyphan@live.com>
2020-12-30 22:31:39 -0800

    clean up console.log in showBebe action

--- src/actions/showBebe.js
+++ src/actions/showBebe.js
@@ -3,7 +3,6 @@ export const fetchABebe = (bebeId, userId) => {
     fetch('http://localhost:3000/api/v1/users/' + userId + '/bebes/'  + bebeId)
     .then(response => response.json())
     .then(bebe => {
-      console.log("connected to fetch a bebe")
       dispatch({type: 'FETCH_A_BEBE', payload: bebe})
     })
   }
