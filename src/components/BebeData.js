0570a87989526664886127179270c8491ad206d0
beckyphan <beckyphan@live.com>
2021-01-01 13:06:22 -0800

    created placeholder for Edit Bebe Form to toggle when editInfo clicked

--- src/components/BebeData.js
+++ src/components/BebeData.js
@@ -5,6 +5,37 @@ class BebeData extends React.Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      editToggle: false
+    }
+  }
+
+  toggleEdit = () => {
+    if (this.state.editToggle) {
+      this.setState({
+        editToggle: false
+      })
+    } else {
+      this.setState({
+        editToggle: true
+      })
+    }
+  }
+
+  displayInfo = () => {
+    return (
+      <p className="align-left">
+        Kind: {this.props.bebe.attributes.kind} <br/>
+        Birthdate: {this.props.bebe.attributes.birthdate} <br/>
+        Bio: {this.props.bebe.attributes.bio} <br/>
+      </p>
+    )
+  }
+
+  displayEditable = () => {
+    return (
+      <p>SHOW EDIT FORM</p>
+    )
   }
 
   render() {
@@ -13,13 +44,8 @@ class BebeData extends React.Component {
 
         <h1>{this.props.bebe.attributes.name}</h1>
         <img src={this.props.bebe.attributes.img} alt="baby pic" className="headshot"/><br/><br/>
-        <button className="edit">Edit Info</button>
-        <p className="align-left">
-          Kind: {this.props.bebe.attributes.kind} <br/>
-          Birthdate: {this.props.bebe.attributes.birthdate} <br/>
-          Bio: {this.props.bebe.attributes.bio} <br/>
-        </p>
-
+        <button className="edit" onClick={this.toggleEdit}>Edit Info</button>
+        {this.state.editToggle ? this.displayEditable() : this.displayInfo()}
         <BebeDays />
 
       </div>
