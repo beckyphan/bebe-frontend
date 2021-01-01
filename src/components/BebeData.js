b97444431571db0448a48f85a9dc25f58e40d4d7
beckyphan <beckyphan@live.com>
2021-01-01 12:56:01 -0800

    add edit info button to BebeData

--- src/components/BebeData.js
+++ src/components/BebeData.js
@@ -2,16 +2,17 @@ import React from 'react';
 import BebeDays from './BebeDays'
 
 const BebeData = (props) => {
-  
+
   return (
     <div className="show-bebe">
 
       <h1>{props.bebe.attributes.name}</h1>
-      <img src={props.bebe.attributes.img} alt="baby pic" className="headshot"/>
+      <img src={props.bebe.attributes.img} alt="baby pic" className="headshot"/><br/><br/>
+      <button className="edit">Edit Info</button>
       <p className="align-left">
         Kind: {props.bebe.attributes.kind} <br/>
         Birthdate: {props.bebe.attributes.birthdate} <br/>
-        Bio: {props.bebe.attributes.bio}
+        Bio: {props.bebe.attributes.bio} <br/>
       </p>
 
       <BebeDays />
