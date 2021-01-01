90dfa2553e5a32c97b4633e060d39142a3f12f4f
beckyphan <beckyphan@live.com>
2021-01-01 13:57:54 -0800

    create EditBebeForm component and drafted handleSubmit action

--- src/components/BebeData.js
+++ src/components/BebeData.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BebeDays from './BebeDays'
 import DisplayBebeInfo from './DisplayBebeInfo'
+import EditBebeForm from './EditBebeForm'
 
 class BebeData extends React.Component {
 
@@ -23,12 +24,6 @@ class BebeData extends React.Component {
     }
   }
 
-  displayEditable = () => {
-    return (
-      <p>SHOW EDIT FORM</p>
-    )
-  }
-
   render() {
     return (
       <div className="show-bebe">
@@ -36,7 +31,7 @@ class BebeData extends React.Component {
         <h1>{this.props.bebe.attributes.name}</h1>
         <img src={this.props.bebe.attributes.img} alt="baby pic" className="headshot"/><br/><br/>
         <button className="edit" onClick={this.toggleEdit}>Edit Info</button>
-        {this.state.editToggle ? this.displayEditable() : <DisplayBebeInfo kind={this.props.bebe.attributes.kind} birthdate={this.props.bebe.attributes.birthdate} bio={this.props.bebe.attributes.bio}/>}
+        {this.state.editToggle ? <EditBebeForm name={this.props.bebe.attributes.name} kind={this.props.bebe.attributes.kind} birthdate={this.props.bebe.attributes.birthdate} bio={this.props.bebe.attributes.bio} img={this.props.bebe.attributes.img} />: <DisplayBebeInfo kind={this.props.bebe.attributes.kind} birthdate={this.props.bebe.attributes.birthdate} bio={this.props.bebe.attributes.bio}/>}
         <BebeDays />
 
       </div>
---
+++ src/components/EditBebeForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+class EditBebeForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: this.props.name,
+      birthdate: this.props.birthdate,
+      kind: this.props.kind,
+      bio: this.props.bio,
+      img: this.props.img
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log("EDIT BEBE FORM SUBMITTED")
+    console.log(event)
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="new-card">
+        <form onSubmit={this.handleSubmit} className='new-bebe'>
+          <div className="align-left">
+          <label>Name: </label>
+          <input type='text' value={this.state.name} name='name' onChange={this.handleChange} /><br/>
+
+          <label>Birthdate: </label>
+          <input type='date' value={this.state.birthdate} name="birthdate" onChange={this.handleChange} /><br/>
+
+          <label>Kind: </label>
+          <select name="kind" value={this.state.kind} onChange={this.handleChange}>
+            <option value="human">Human</option>
+            <option value="plant">Plant</option>
+            <option value="pet">Pet</option>
+          </select><br/>
+
+          <label>Bio: </label>
+          <textarea value={this.state.bio} name="bio" onChange={this.handleChange}/><br/>
+
+          <label>Img URL: </label>
+          <input type='text' value={this.state.img} name="img" onChange={this.handleChange} /><br/>
+
+          </div>
+          <input type='submit' className="center-button"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default EditBebeForm
