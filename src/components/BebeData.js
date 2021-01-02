3f26f7a1ce8e65ce68db99ca5b47c831bf6c087d
beckyphan <beckyphan@live.com>
2021-01-02 15:51:37 -0800

    initial edit handled by BebeData to display EditBebeForm

--- src/components/BebeData.js
+++ src/components/BebeData.js
@@ -31,7 +31,7 @@ class BebeData extends React.Component {
 
         <h1>{this.props.bebe.attributes.name}</h1>
         <img src={this.props.bebe.attributes.img} alt="baby pic" className="headshot"/><br/><br/>
-        <button className="edit" onClick={this.toggleEdit}>Edit Info</button>
+        {!this.state.editToggle ? <button className="edit" onClick={this.toggleEdit}>Edit Info</button> : null}
         {this.state.editToggle ? <EditBebeForm user={this.props.bebe.relationships.user.data} name={this.props.bebe.attributes.name} kind={this.props.bebe.attributes.kind} birthdate={this.props.bebe.attributes.birthdate} bio={this.props.bebe.attributes.bio} img={this.props.bebe.attributes.img} bebeId={this.props.bebe.id} />: <DisplayBebeInfo kind={this.props.bebe.attributes.kind} birthdate={this.props.bebe.attributes.birthdate} bio={this.props.bebe.attributes.bio}/>}
         <BebeDays />
 
