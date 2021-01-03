37ec6be237584ddcb685e7b599a2e40d851adbcb
beckyphan <beckyphan@live.com>
2021-01-03 01:57:11 -0800

    can create bebe but BebeContainer does not re-render Bebes

--- src/reducers/bebeReducer.js
+++ src/reducers/bebeReducer.js
@@ -4,6 +4,10 @@ export default function bebeReducer(state = [], action) {
       case 'FETCH_BEBES':
         return action.payload.data
 
+      case 'CREATE_BEBE':
+        state.push(action.payload.data)
+        return state
+
       default:
         return state
   }
