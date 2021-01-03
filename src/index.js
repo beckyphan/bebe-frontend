b098c71a8e88e22508a32914dadf5eeffe331f06
beckyphan <beckyphan@live.com>
2021-01-03 00:05:18 -0800

    refactored userReducer to use combineReducers, to separate other cases

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom'
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import App from './App';
 import userReducer from './reducers/userReducer'
+const rootReducer = combineReducers({
+  user: userReducer,
+})
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(userReducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,58 +1,18 @@
-export default function userReducer(state = {user: [], bebes: [], bebe: {}}, action) {
+export default function userReducer(state = {}, action) {
 
   switch(action.type) {
     case 'LOGIN_USER':
       return {
         ...state,
-        user: action.payload.data
+        id: action.payload.data.id,
+        attributes: action.payload.data.attributes
       }
 
     case 'REGISTER_USER':
       return {
         ...state,
-        user: action.payload.data
-      }
-
-    case 'FETCH_BEBES':
-      return {
-        ...state,
-        bebes: action.payload.data
-      }
-
-    case 'CREATE_BEBE':
-      return {
-        ...state,
-        bebes: [...state.bebes, action.payload.data]
-      }
-
-    case 'EDIT_BEBE':
-        return {
-          ...state,
-          bebes: state.bebes.map(bebe => bebe.id === action.payload.data.id ? action.payload.data : bebe)
-        }
-
-    case 'FETCH_A_BEBE':
-      return {
-        ...state,
-        bebe: action.payload.data
-      }
-
-    case 'DELETE_BEBE':
-      return {
-        ...state,
-        bebes: state.bebes.filter(bebe => bebe.id !== action.payload.data.id)
-      }
-
-    case 'FETCH_BEBE_DAYS':
-      return {
-        ...state,
-        days: action.payload.data
-      }
-
-    case 'CREATE_DAY':
-      return {
-        ...state,
-        days: [...state.days, action.payload.data]
+        id: action.payload.data.id,
+        attributes: action.payload.data.attributes
       }
 
     default:
