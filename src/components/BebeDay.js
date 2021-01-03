b37e77f81fbe2e8cb8b2ea964e3cd508720c57f8
beckyphan <beckyphan@live.com>
2021-01-02 16:16:23 -0800

    drafted showData toggle in BebeDay component

--- src/components/BebeDay.js
+++ src/components/BebeDay.js
@@ -23,9 +23,18 @@ class BebeDay extends React.Component {
   }
 
   render() {
+    let body;
+
+    if (this.state.showData) {
+      body = <div>SHOW DATA HERE</div>
+    } else {
+      body = null;
+    }
+
     return (
       <div className="day">
         <button onClick={this.handleClick}>{this.props.date}</button>
+        {body}
       </div>
     )
   }
