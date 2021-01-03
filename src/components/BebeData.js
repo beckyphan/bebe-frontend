6b19e614ebb199c4b81bebd80e0d18f58b9e5fb6
beckyphan <beckyphan@live.com>
2021-01-02 18:41:47 -0800

    changed submit button to a fleshed out form for new Day creation

--- src/components/BebeData.js
+++ src/components/BebeData.js
@@ -10,7 +10,7 @@ class BebeData extends React.Component {
 
     this.state = {
       editToggle: false,
-      dayToggle: false
+      newDayData: ""
     }
   }
 
@@ -21,10 +21,35 @@ class BebeData extends React.Component {
     })
   }
 
-  createDay = () => {
-    let newBebeDay = {
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    let newBebeDay = {
+      bebe_id: this.props.bebe.id,
+      note: "",
+      date: this.state.newDayData
     }
+
+    fetch('http://localhost:3000/api/v1/users/' + this.props.bebe.relationships.user.data.id + '/bebes/' + this.props.bebe.id + '/days', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newBebeDay)
+      })
+    .then(response => response.json())
+    .then(day => {
+      this.setState({
+        ...this.state,
+        newDayData: ""
+      })
+    })
   }
 
   render() {
@@ -35,9 +60,13 @@ class BebeData extends React.Component {
         <img src={this.props.bebe.attributes.img} alt="baby pic" className="headshot"/><br/><br/>
         {!this.state.editToggle ? <button className="edit" onClick={this.toggleEdit}>Edit Info</button> : null}
         {this.state.editToggle ? <EditBebeForm user={this.props.bebe.relationships.user.data} name={this.props.bebe.attributes.name} kind={this.props.bebe.attributes.kind} birthdate={this.props.bebe.attributes.birthdate} bio={this.props.bebe.attributes.bio} img={this.props.bebe.attributes.img} bebeId={this.props.bebe.id} />: <DisplayBebeInfo kind={this.props.bebe.attributes.kind} birthdate={this.props.bebe.attributes.birthdate} bio={this.props.bebe.attributes.bio}/>}
-        <BebeDays />
-        <button className="addDay" onClick={this.createDay}> + </button>
 
+        <BebeDays/>
+
+        <form className="addDay" onSubmit={this.handleSubmit}>
+          <input type="date" name="newDayData" value={this.state.newDayData} onChange={this.handleChange} />
+          <input type="submit" value="+"/>
+        </form>
       </div>
     )
   }
