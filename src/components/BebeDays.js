86fb4f9bced9bcdcbab58f1b975140c2d93baf49
beckyphan <beckyphan@live.com>
2021-01-03 04:16:57 -0800

    display partial # of days data unless user clicks show more

--- src/components/BebeDays.js
+++ src/components/BebeDays.js
@@ -4,9 +4,22 @@ import BebeDay from './BebeDay'
 
 class BebeDays extends React.Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      daysShown: 5
+    }
+  }
 
   displayDay = (day) => {
     return <BebeDay key={day.id} date={day.attributes.date}/>
+  }
+
+  handleClick = (prevState) => {
+    this.setState(prevState => {
+      return {daysShown: prevState.daysShown + 5}
+    })
   }
 
   render() {
@@ -15,13 +28,16 @@ class BebeDays extends React.Component {
     if (this.props.days.length === 0) {
       body = <p>No Data Collected Yet!</p>
     } else {
-      body = this.props.days.map(day => this.displayDay(day))
+      this.props.days.sort((a, b) => (a.attributes.date > b.attributes.date) ? 1 : -1)
+      let allDays = this.props.days.map(day => this.displayDay(day))
+      body = allDays.slice(0, this.state.daysShown)
     }
 
     return (
       <div>
         <h3> Days </h3>
         { body }
+        { this.state.daysShown > this.props.days.length ? null : <button className="showMore" onClick={this.handleClick}>Show More</button>}
       </div>
     )
   }
