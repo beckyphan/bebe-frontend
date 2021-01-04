5b827b00103c5a2bec7cf578edda23dba60f0bff
beckyphan <beckyphan@live.com>
2021-01-03 22:49:49 -0800

    deleteTracking action + dispatch case created; will move w/in component

--- src/reducers/trackingReducer.js
+++ src/reducers/trackingReducer.js
@@ -4,6 +4,9 @@ export default function trackingReducer(state = [], action) {
     case 'FETCH_TRACKINGS':
       return action.payload.data
 
+    case 'DELETE_TRACKING':
+      return state.filter(tracking => tracking.id !== action.payload.data.id)
+
     default:
       return state
   }
