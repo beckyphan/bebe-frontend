7e654fe92d248ab8fa3ecb10e324c6e0c8815b61
beckyphan <beckyphan@live.com>
2021-01-09 23:24:24 -0800

    corrected conditional if 5 days, no need to showMore button

--- src/components/BebeDays.js
+++ src/components/BebeDays.js
@@ -38,7 +38,7 @@ class BebeDays extends React.Component {
         <h3> Days </h3>
         { body }
         <br/><br/>
-        { this.state.daysShown > this.props.days.length ? null : <button className="showMore" onClick={this.handleClick}>Show More</button>}
+        { this.state.daysShown >= this.props.days.length ? null : <button className="showMore" onClick={this.handleClick}>Show More</button>}
       </div>
     )
   }
