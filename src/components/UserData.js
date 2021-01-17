0f8e9f69a2dae979641f08d789ebbcab530768d2
beckyphan <beckyphan@live.com>
2021-01-16 16:55:44 -0800

    moved User's Data Display into a presentational functional component

---
+++ src/components/UserData.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const UserData = (props) => {
+
+  return (
+    <div className="profile">
+      <p>Name: {props.name}</p>
+      <p>Username: {props.username}</p>
+      <br/>
+      <p>Total Number of Bébés: {props.bebesNumber}</p>
+      <ul>
+        <li>Humans: {props.humansNum}</li>
+        <li>Pets: {props.petsNum}</li>
+        <li>Plants: {props.plantNum}</li>
+      </ul>
+
+    </div>
+  )
+
+}
+
+export default UserData
