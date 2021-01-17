7551a49fab70e55afb9894c7f3ea494e658c6a96
beckyphan <beckyphan@live.com>
2021-01-16 16:07:52 -0800

    installed redux-persist to retain redux store even when refreshing page

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App';
 import userReducer from './reducers/userReducer'
 import bebeReducer from './reducers/bebeReducer'
@@ -18,13 +21,23 @@ const rootReducer = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+let store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)))
+let persistor = persistStore(store)
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
