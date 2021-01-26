9903ac5f910723644770b32235c72c7bef8907ce
Becky Phan <beckyphan@live.com>
2021-01-26 14:26:40 -0800

    changed Bebes to class component; adding name-sorting toggle button

--- src/components/Bebes.js
+++ src/components/Bebes.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import BebeCard from './BebeCard'
 
-const Bebes = (props) => {
+class Bebes extends React.Component {
 
-  return (
-    <div className="dashboard">
-      {props.bebes.length > 0 ? props.bebes.map(bebe => <BebeCard key={bebe.id} bebe={bebe} user={props.user}/>): <p>No Children Yet!</p>}
-    </div>
-  )
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      bebesArray: [],
+      toggle: false
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      bebesArray: this.props.bebes
+    })
+  }
+
+  handleClick = () => {
+    let sortedArray = this.props.bebes.slice()
+    sortedArray.sort((a, b) => (a.attributes.name.toLowerCase() > b.attributes.name.toLowerCase()) ? 1 : -1)
+    if (!this.state.toggle) {
+        this.setState({
+        bebesArray: sortedArray,
+        toggle: true
+      })
+    } else {
+      this.setState({
+        bebesArray: this.props.bebes,
+        toggle: false
+      })
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <span><button onClick={this.handleClick}>SORT</button></span>
+        <div className="dashboard">
+          {this.props.bebes.length > 0 ? this.state.bebesArray.map(bebe => <BebeCard key={bebe.id} bebe={bebe} user={this.props.user}/>) : <p>No Children Yet!</p>}
+        </div>
+      </>
+    )
+  }
 }
 
 export default Bebes
